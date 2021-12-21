1bf1df2bf1157df5b29cc283a995595ae2ad4447
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2021-12-21 14:50:58 +0700

    Update core.py

--- core.py
+++ core.py
@@ -9,7 +9,7 @@ import streamlit as st
 st.title("Webcam Live Feed")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(CAP_V4L2)
 
 while run:
     _, frame = camera.read()
