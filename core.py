2f22d14355ee0a2d8d8a1b6b50024e0f6bbf068e
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2021-12-21 15:03:26 +0700

    Update core.py

--- core.py
+++ core.py
@@ -9,8 +9,8 @@ import streamlit as st
 st.title("Webcam Live Feed")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(1)
-
+camera = cv2.VideoCapture(0)
+print(camera)
 while run:
     _, frame = camera.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
